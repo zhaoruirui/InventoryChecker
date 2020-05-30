ef0e51d6479eb2a6feeaba919d5842129a6d14bf
Rui Zhao <rui.zhao@chengage.com>
2020-05-29 21:54:23 -0400

    use title instead of url for display  reorder when accessed

--- content.js
+++ content.js
@@ -3,20 +3,25 @@ chrome.storage.local.get(["browseHistory"], function(items){
         items.browseHistory = [];
     }
     let alreadyExist = false;
+    let replacingIndex = -1;
     let currentUrl = window.location.href;
-    items.browseHistory.forEach(function (value) {
+    items.browseHistory.forEach(function (value, index) {
 
-        if(value.indexOf(currentUrl)>-1){
+        if(value.indexOf(currentUrl)>-1 && !alreadyExist){
             alreadyExist = true;
+            replacingIndex = index;
         }
     })
-    if(!alreadyExist){
-        items.browseHistory.push(window.location.href);
-        if(items.browseHistory.length>10){
-            items.shift();
-        }
-        chrome.storage.local.set({ "browseHistory": items.browseHistory }, function(){
-            //  Data's been saved boys and girls, go on home
-        });
+    if(alreadyExist){
+        items.browseHistory.splice(replacingIndex, 1);
     }
+
+    items.browseHistory.push(window.location.href+ "::" +document.title);
+    if(items.browseHistory.length>10){
+        items.shift();
+    }
+    chrome.storage.local.set({ "browseHistory": items.browseHistory }, function(){
+        //  Data's been saved boys and girls, go on home
+    });
+
 });
--- popup.js
+++ popup.js
@@ -1,11 +1,12 @@
 chrome.storage.local.get(["browseHistory"], function(items){
-    items.browseHistory.forEach(function (value) {
+    items.browseHistory.reverse().forEach(function (value) {
+        let names = value.split("::");
         let a = document.createElement('a');
         let x = document.createElement("LI");
-        let linkText = document.createTextNode(value);
+        let linkText = document.createTextNode(names[1]);
         a.appendChild(linkText);
-        a.title = value;
-        a.href = value;
+        a.title = names[1];
+        a.href = names[0];
         x.appendChild(a)
         document.querySelector('.parentList').appendChild(x)
     })
