1c947a8e87044adbf8c8dad480a49a2d186281dd
Rui Zhao <rui.zhao@chengage.com>
2020-06-01 17:32:20 -0400

    ebay snipper

--- content.js
+++ content.js
@@ -1,27 +1,28 @@
-chrome.storage.local.get(["browseHistory"], function(items){
-    if(Object.keys(items.browseHistory).length===0){
-        items.browseHistory = [];
-    }
-    let alreadyExist = false;
-    let replacingIndex = -1;
-    let currentUrl = window.location.href;
-    items.browseHistory.forEach(function (value, index) {
-
-        if(value.indexOf(currentUrl)>-1 && !alreadyExist){
-            alreadyExist = true;
-            replacingIndex = index;
+let callCount = 0;
+let url = "https://pay.ebay.com/rxo?action=create&rypsvc=true&pagename=ryp&TransactionId=-1&shippingcode=USPSPriority&buyerselectedsc=false&item=";
+let id1 = "123832525898";
+let id2 = "123839721136";
+let goodStatus = "success";
+function check(){
+    jQuery.get(url + id1, function(data, status){
+        if(status===goodStatus && data.indexOf("CONFIRM_AND_PAY_BUTTON")>-1){
+            console.log('added to chart: '+'123832525898' );
+            console.log(data);
+        }else{
+            jQuery.get(url + id2, function(data, status){
+                if(status===goodStatus && data.indexOf("CONFIRM_AND_PAY_BUTTON")>-1){
+                    console.log('added to chart: '+'122631362598' );
+                    console.log(data);
+                }
+            });
         }
-    })
-    if(alreadyExist){
-        items.browseHistory.splice(replacingIndex, 1);
-    }
 
-    items.browseHistory.push(window.location.href+ "::" +document.title);
-    if(items.browseHistory.length>10){
-        items.shift();
-    }
-    chrome.storage.local.set({ "browseHistory": items.browseHistory }, function(){
-        //  Data's been saved boys and girls, go on home
+        if(callCount%100===0){
+            console.log(callCount++);
+        }
     });
-
-});
+}
+let timer = setInterval(check, 4000);
+jQuery(window).unload( function () {
+    clearInterval(timer);
+} );
