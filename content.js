5f7327b77fd751cf0d4cdd83e2fd70f8c24965e6
Rui Zhao <rui.zhao@chengage.com>
2020-06-04 07:05:44 -0400

    working version

--- content.js
+++ content.js
@@ -1,34 +1,16 @@
-let url = "https://pay.ebay.com/rxo?action=create&rypsvc=true&pagename=ryp&TransactionId=-1&shippingcode=USPSPriority&buyerselectedsc=false&item=";
+let url = "https://pay.ebay.com/rxo?action=create&rypsvc=true&pagename=ryp&TransactionId=-1&shippingcode=USPSPriority&buyerselectedsc=false&item=123832525898";
 // let id1 = "124024800061";
 let id1 = "123832525898";
 let id2 = "123839721136";
 let goodStatus = "success";
 
-if("https://pay.ebay.com/rxo?action=view&sessionid="===window.location.href){
-    function check(){
-        jQuery.get(url + id1, function(data, status){
-            console.log("called out")
-            if(status===goodStatus && data.indexOf("CONFIRM_AND_PAY_BUTTON")>-1){
-                console.log('added to chart: '+id1 );
-                clearInterval(timer);
-                // chrome.tabs.create({ url: url + id1 });
-                window.location.href = url+id1;
-            }
-        });
-        jQuery.get(url + id2, function(data, status){
-            if(status===goodStatus && data.indexOf("CONFIRM_AND_PAY_BUTTON")>-1){
-                console.log('added to chart: '+id2 );
-                clearInterval(timer);
-                // chrome.tabs.create({ url: url + id2 });
-                window.location.href = url+id2;
-            }
-        });
+
+jQuery(".confirm-and-pay-wrapper button").click();
+
+if($(".text-display").html().indexOf("isn't available")){
+    function reload(){
+        console.log("reload");
+        window.location.href = url;
     }
-    let timer = setInterval(check, 2000);
-    jQuery(window).unload( function () {
-        clearInterval(timer);
-    } );
-}else if(jQuery(".confirm-and-pay-wrapper button").length>0){
-    console.log("confirming payment" );
-    jQuery(".confirm-and-pay-wrapper button").click();
+    setTimeout(reload, 500);
 }
