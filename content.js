a4b75c2f7a76099618a09883963dff24d12820cb
Rui Zhao <rui.zhao@chengage.com>
2020-06-09 19:59:12 -0400

    fixed last item id and changed to 350ms

--- content.js
+++ content.js
@@ -18,7 +18,8 @@ if(window.location.href==="https://pay.ebay.com/rxo?action=view&sessionid="){
     select.options.add( new Option("Acer cb5 113838275752","113838275752") );
     select.options.add( new Option("HP-f272 113835595827","113835595827") );
     select.options.add( new Option("HP -ca051 113146650470","113146650470") );
-    select.options.add( new Option("acer cb5132 122631362598","123732155109") );
+    select.options.add( new Option("acer cb5132 123732155109","123732155109") );
+    select.options.add( new Option("HP-v020 122631362598","122631362598") );
 
     let itemId ="123832525898";
     button.addEventListener("click", function () {
@@ -37,7 +38,7 @@ if(window.location.href==="https://pay.ebay.com/rxo?action=view&sessionid="){
                 window.location.href = url + data.itemId;
             });
         }
-        setTimeout(reload, 500);
+        setTimeout(reload, 350);
     }
 }
 
